c77839d4bad78338c850e5c957f50f4d4501de8a
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 01:43:53 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,6 +4,18 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const expect = chai.expect
+let server
+
+before(function(done) {
+  return app.up().then(_server => {
+    server = _server
+    done()
+  })
+})
+
+after(function() {
+  server.close()
+})
 
 describe('app', function() {
   describe('up', function() {
